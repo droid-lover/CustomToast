11c0a4c3f2875266d4ad4149b0ea71980db12fcc
myJarvis <sachin@bmsils.com>
2016-03-25 17:58:57 +0530

    first commit

---
+++ app/src/main/java/com/myjarvis/mytoast/MyCustomToast.java
@@ -0,0 +1,34 @@
+package com.myjarvis.mytoast;
+
+import android.content.Context;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+/**
+ * Created by jarvis on 25-Mar-16
+ * at  5:33 PM .
+ */
+
+//jarvis property by god grace
+public class MyCustomToast {
+    public static void show(Context context, String text, boolean isLong) {
+        LayoutInflater inflater = LayoutInflater.from(context);
+        View layout = inflater.inflate(R.layout.toast_item, null);
+
+        ImageView image = (ImageView) layout.findViewById(R.id.toast_image);
+        image.setImageResource(R.mipmap.ic_launcher);
+
+        TextView textV = (TextView) layout.findViewById(R.id.toast_text);
+        textV.setText(text);
+
+        Toast toast = new Toast(context);
+        toast.setGravity(Gravity.CENTER, 0, 0);
+        toast.setDuration((isLong) ? Toast.LENGTH_LONG : Toast.LENGTH_SHORT);
+        toast.setView(layout);
+        toast.show();
+    }
+}
