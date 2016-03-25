0f8fd95cdf64e43931e5572d75836ecfe15fd8ba
myJarvis <sachin@bmsils.com>
2016-03-25 18:11:36 +0530

    refined

--- app/src/main/java/com/myjarvis/mytoast/MainActivity.java
+++ app/src/main/java/com/myjarvis/mytoast/MainActivity.java
@@ -3,13 +3,11 @@ package com.myjarvis.mytoast;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
 import android.widget.Button;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,35 +26,18 @@ public class MainActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         setSupportActionBar(mainActivityToolbar);
 
-        generateToastButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                MyCustomToast.show(MainActivity.this, "This is myCustomToast ", true);
-            }
-        });
+//        generateToastButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//                MyCustomToast.show(MainActivity.this, "This is myCustomToast ", true);
+//            }
+//        });
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
+  @OnClick(R.id.generateToastButton)
+    public void generateToastButtonClicked(){
+      MyCustomToast.show(MainActivity.this, "This is myCustomToast ", true);
+  }
 }
